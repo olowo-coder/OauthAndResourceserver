2d8e0131aa6bc978e211c3ccd1e997e073b51a60
olowo-coder <#GODisgreat100>
2022-07-27 22:39:58 +0100

    Oauth2 and resource server

---
+++ OtherresourceServer/src/main/java/com/example/otherresourceserver/OtherresourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.otherresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtherresourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtherresourceServerApplication.class, args);
+    }
+
+}
---
+++ OtherresourceServer/src/test/java/com/example/otherresourceserver/OtherresourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.otherresourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtherresourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
