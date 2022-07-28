f9c81599a626b99c4c860c7b9fc6b2edaf0ae14a
olowo-coder <#GODisgreat100>
2022-07-28 19:16:53 +0100

    Oauth2 and resource server

--- ResourceServerT/src/main/java/com/example/resourceservert/config/AppConfig.java
+++ ResourceServerT/src/main/java/com/example/resourceservert/config/AppConfig.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -14,6 +16,8 @@ import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AppConfig {
 
 //    @Value("${oauth2.check-token-url}")
@@ -32,7 +36,8 @@ public class AppConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests(auths -> auths
-                        .antMatchers(HttpMethod.GET, "/resource").hasAuthority("SCOPE_lion.read").anyRequest()
+                        .antMatchers(HttpMethod.GET, "/resource")
+//                        .hasAuthority("SCOPE_lion.read").anyRequest()
                         .authenticated())
                 .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return http.build();
--- ResourceServerT/src/main/java/com/example/resourceservert/controller/ResourceController.java
+++ ResourceServerT/src/main/java/com/example/resourceservert/controller/ResourceController.java
@@ -2,6 +2,7 @@ package com.example.resourceservert.controller;
 
 import com.example.resourceservert.dto.ResponseDTO;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.Map;
 @RequestMapping(value = "/resource")
 public class ResourceController {
 
+    @PreAuthorize("hasAuthority('SCOPE_lion.read')")
     @GetMapping(name  = "from-resourse")
     public ResponseEntity<?> accessData(){
         return ResponseEntity.ok(ResponseDTO.builder().data("Successful").build());
